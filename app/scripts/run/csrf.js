981df4ea7a87343a80a3bb9f792dc708e3ae1383
stefannygard <stefan_nygard@hotmail.com>
2014-09-23 10:02:47 +0200

    correcting csrf headers

--- app/scripts/run/csrf.js
+++ app/scripts/run/csrf.js
@@ -1,6 +1,6 @@
 'use strict';
 angular.module('mydrawingsApp').run(['$rootScope', '$http', 'CSRF_TOKEN', 
   function($rootScope, $http, CSRF_TOKEN) {
-    $http.defaults.headers.post['X-CSRFToken'] = CSRF_TOKEN;
+    //$http.defaults.headers.post['X-CSRFToken'] = CSRF_TOKEN;
   }
 ]);
