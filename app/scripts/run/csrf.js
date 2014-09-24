439e17602bdf069d07b40d966380ac11519629ba
stefannygard <stefan_nygard@hotmail.com>
2014-09-24 08:45:35 +0200

    with credentials on

--- app/scripts/run/csrf.js
+++ app/scripts/run/csrf.js
@@ -1,6 +1,7 @@
 'use strict';
 angular.module('mydrawingsApp').run(['$rootScope', '$http', 'CSRF_TOKEN', 
   function($rootScope, $http, CSRF_TOKEN) {
+    $http.defaults.withCredentials = true;
     $http.defaults.headers.post['X-CSRF-Token'] = CSRF_TOKEN;
   }
 ]);
